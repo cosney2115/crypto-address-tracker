b9a86e6a8d140dd01d05b1d73aae9b144a634254
Cosney <mateuszolsz29@gmail.com>
2026-01-22 21:04:11 +0100

    added more pub

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,15 @@ if (!ltcXpub) throw new Error("XPUB_LTC is not defined");
 
 const ltcAddress = generateAddress(ltcXpub, "ltc", 0);
 console.log(ltcAddress);
+
+const btcXpub = env.XPUB_BTC;
+if (!btcXpub) throw new Error("XPUB_BTC is not defined");
+
+const btcAddress = generateAddress(btcXpub, "btc", 0);
+console.log(btcAddress);
+
+const dogeXpub = env.XPUB_DOGE;
+if (!dogeXpub) throw new Error("XPUB_DOGE is not defined");
+
+const dogeAddress = generateAddress(dogeXpub, "doge", 0);
+console.log(dogeAddress);
